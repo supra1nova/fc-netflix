af51367f8e6830acfddfbd7b2ee0f70674b7111e
supra1nova <supra1nova@gmail.com>
2025-04-23 21:39:41 +0900

    Feat: CreateMovieDto, UpdateMovieDto 기본 validation 적용
    -class-validation, class-transformer

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,30 @@
+import { IsOptional, IsString, Validate, ValidationArguments, ValidatorConstraint, ValidatorConstraintInterface } from 'class-validator';
+import { Transform } from 'class-transformer';
+
+enum MovieGenre {
+  FNTS = 'fantasy',
+  HRRR = 'horror',
+  ACTN = 'action',
+}
+
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(value: any, validationArguments?: ValidationArguments): Promise<boolean> | boolean {
+    // 비밀번호 길이는 4-8
+    return value.length > 4 && value.length < 8;
+  }
+
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4-8자 여야 합니다. ($value)';
+  }
+}
+
 export class UpdateMovieDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+
+  @IsString()
+  @IsOptional()
   genre?: string;
 }
