e58318cc31c7057ce1d38cbaadf6b5133d13e013
supra1nova <supra1nova@gmail.com>
2025-07-09 23:02:29 +0900

    Feat: ConfigModule 및 TypeOrmModule 적용
    -ConfigModule 은 환경변수 로딩 위해
    -TypeOrmModule 은 DB 설정 로딩 위해

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common'
 import { MovieModule } from './movie/movie.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
   imports: [MovieModule],
+  imports: [
+    // 환경변수를 로딩 후 애플케이션 전역에서 사용할 수 있도록 설정
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
