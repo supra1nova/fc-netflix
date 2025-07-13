551968b6559fcc9de70346543246de559fb5cdf5
supra1nova <supra1nova@gmail.com>
2025-07-13 15:13:50 +0900

    Feat: Genre 모듈 추가 및 crud 구현, 그리고 Movie 와 ManyToMany 관계 설정
    -Genre 및 Movie 에 @ManyToMany() 관계 적용
    -다대다 관계만 @JoinTable() 설정이 가능하며, 소유자(owner) 에만 설정 가능

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { Movie } from './movie/entity/movie.entity'
 import { MovieDetail } from './movie/entity/movie-detail.entity'
 import { DirectorModule } from './director/director.module'
 import { Director } from './director/entity/director.entity'
+import { GenreModule } from './genre/genre.module'
+import { Genre } from './genre/entities/genre.entity'
 
 @Module({
   imports: [
@@ -33,13 +35,14 @@ import { Director } from './director/entity/director.entity'
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
         // entity: [/*'src/!**!/!*.entity{.ts,.js}'*/],
-        entities: [Movie, MovieDetail, Director],
+        entities: [Movie, MovieDetail, Director, Genre],
         synchronize: true,
       }),
       inject: [ConfigService],
     }),
     MovieModule,
     DirectorModule,
+    GenreModule,
   ],
 })
 export class AppModule {}
