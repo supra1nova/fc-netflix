670f20d27e1aed871d9181978086829a89aca080
supra1nova <supra1nova@gmail.com>
2025-07-17 07:46:52 +0900

    Fix: base entity serialization
    -클로벌에 app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector))); 로 적용하거나 클래스 또는 라우터에 @UseInterceptors(ClassSerializerInterceptor) 를 적용한 뒤 Dto나 entity에 @Expose(), @Exclude() 등을 적용
    -@Exclude()나 @Expose() 를 공통 base entity class 자체에 걸면 class-transformer가 상속된 클래스에서 데코레이터를 인식하지 못하는 문제가 있음 → 각 필드에 개별적으로 걸어야함

--- src/common/entity/base.entity.ts
+++ src/common/entity/base.entity.ts
@@ -1,12 +1,16 @@
 import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm'
+import { Exclude } from 'class-transformer'
 
 export class BaseTable {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date
 
   @VersionColumn()
+  @Exclude()
   version: number
 }
