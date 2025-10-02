3d7ea2996378616aaf76798e5466840a09283b3a
supra1nova <supra1nova@gmail.com>
2025-10-02 19:19:55 +0900

    Feature: cursor형 pagination 응용2 - 동적 where절 OR/AND 활용한 개별 항목 ASC/DESC 적용

--- src/common/module/common.service.ts
+++ src/common/module/common.service.ts
@@ -17,7 +17,11 @@ export class CommonService {
     qb.orderBy(`${qb.alias}.id`, order)
   }
 
-  applyCursorPaginationParamsToQb<T extends ObjectLiteral>(qb: SelectQueryBuilder<T>, dto: CursorPaginationDto) {
+  /**
+   * 복합 비교(튜플 비교) -> 일괄 ASC/DESC 적용시 사용
+   * (column1, column2, column3) > (value1, value2, value3)
+   */
+  applyComplexComparisonCursorPaginationParamsToQb<T extends ObjectLiteral>(qb: SelectQueryBuilder<T>, dto: CursorPaginationDto) {
     let { cursor, order, take } = dto
 
     if (cursor) {
@@ -55,6 +59,64 @@ export class CommonService {
         qb.addOrderBy(`${qb.alias}.${column}`, direction)
       }
     }
+
+    qb.take(take)
+  }
+
+  /**
+   * OR-AND 누적 조건 생성 -> 개별 ASC/DESC 적용시 사용
+   * WHERE (column1 > value1)
+   *    OR (column1 = value1 AND column2 < value2)
+   *    OR (column1 = value1 AND column2 = value2 AND column3 = value3)
+   *    OR (column1 = value1 AND column2 = value2 AND column3 < value3 AND column4 = value4)
+   */
+  applyCursorPaginationParamsToQb<T extends ObjectLiteral>(qb: SelectQueryBuilder<T>, dto: CursorPaginationDto) {
+    let { cursor, order, take } = dto
+
+    if (cursor) {
+      const decodedCursor = Buffer.from(cursor, 'base64').toString('utf-8')
+      const cursorObj = JSON.parse(decodedCursor)
+
+      order = cursorObj.order
+      const { values } = cursorObj
+
+      const conditions: string[] = []
+      const params: Record<string, any> = {}
+      let prevCondition: string[] = []
+
+      // WHERE 절 처리
+      order.forEach((o) => {
+        const [col, dir] = o.split('_')
+        const aliasCol = `${qb.alias}.${col}`
+
+        const operator = dir === 'DESC' ? '<' : '>'
+        // 이전 컬럼 = 값 조건 + 현재 비교 컬럼 조건
+        const currentCondition = [...prevCondition, `${aliasCol} ${operator} :${col}`]
+
+        conditions.push(`(${currentCondition.join(' AND ')})`)
+        params[col] = values[col]
+
+        // 다음 단계에서 사용할 prevCondition 업데이트 (동일 컬럼 = 값)
+        prevCondition.push(`${aliasCol} = :${col}`)
+      })
+
+      qb.andWhere(conditions.join(' OR '), params)
+    }
+
+    // ORDER BY 처리
+    order.forEach((o, i) => {
+      const [column, direction] = o.split('_')
+
+      if (direction !== 'ASC' && direction !== 'DESC') {
+        throw new BadRequestException('Order에 ASC 또는 DESC 를 입력해주세요')
+      }
+
+      if (i < 1) {
+        qb.orderBy(`${qb.alias}.${column}`, direction as 'ASC' | 'DESC')
+      } else {
+        qb.addOrderBy(`${qb.alias}.${column}`, direction as 'ASC' | 'DESC')
+      }
+    })
 
     qb.take(take)
   }
