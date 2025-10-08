05e7333fe87f16664e9dba1e93d5cf2ad77a7eeb
supra1nova <supra1nova@gmail.com>
2025-10-08 21:17:35 +0900

    Feature: ApiHideProperty 적용

--- src/common/entity/base.entity.ts
+++ src/common/entity/base.entity.ts
@@ -1,16 +1,20 @@
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm'
 import { Exclude } from 'class-transformer'
+import { ApiHideProperty } from '@nestjs/swagger'
 
 export class BaseTable {
   @CreateDateColumn()
   @Exclude()
+  @ApiHideProperty()
   createdAt: Date
 
   @UpdateDateColumn()
   @Exclude()
+  @ApiHideProperty()
   updatedAt: Date
 
   @VersionColumn()
   @Exclude()
+  @ApiHideProperty()
   version: number
 }
