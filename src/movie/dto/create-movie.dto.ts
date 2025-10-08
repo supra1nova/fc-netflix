178c9dfc760f5b0cb2a0fdcd96d6c185b81b3e0f
supra1nova <supra1nova@gmail.com>
2025-10-08 20:05:41 +0900

    Feature: ApiProperty 적용

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,18 +1,31 @@
 import { ArrayNotEmpty, IsArray, IsNotEmpty, IsNumber, IsString } from 'class-validator'
 import { Type } from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreateMovieDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 제목',
+    example: '겨울왕국',
+  })
   title: string
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 설명',
+    example: '3시간 휴가쥬?ㅎㅎ',
+  })
   detail: string
 
   @IsNotEmpty()
   @Type(() => Number)
   @IsNumber()
+  @ApiProperty({
+    description: '감독 객체 ID',
+    example: 1,
+  })
   directorId: number
 
   @IsArray()
@@ -24,9 +37,17 @@ export class CreateMovieDto {
       each: true, // each true 인 경우 배열 내부의 값을 모두 검증
     },
   )
+  @ApiProperty({
+    description: '장르 객체 ID 배열',
+    example: [5, 6],
+  })
   genreIds: number[]
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty({
+    description: '영화 파일 이름',
+    example: 'aaa-bbb-ccc-dddd',
+  })
   movieFileName: string
 }
--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types'
+/** @nestjs/mapped-types 의 기본 내장 PartialType을 이용하면 당연히 작동하는게 정상이지만
+ *  객체가 swagger 에서 빈값으로 나오며 인식되지 않음 -> @nestjs/swagger 에서 import 하면 같은 기능이지만 swagger에서 인식함
+ */
+// import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 import { CreateMovieDto } from './create-movie.dto'
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
