efd93603305cc283a82e4f10864aa985e66bbaa9
supra1nova <supra1nova@gmail.com>
2025-10-13 15:44:11 +0900

    Fix: e2e 테스트 구동 시 NODE_ENV=test 설정, movie e2e 테스트 위한 seeding 설정

--- src/movie/movie.e2e.spec.ts
+++ src/movie/movie.e2e.spec.ts
@@ -3,11 +3,24 @@ import { INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
 import { App } from 'supertest/types'
 import { AppModule } from '../app.module'
+import { User } from '../user/entities/user.entity'
+import { Movie } from './entity/movie.entity'
+import { Director } from '../director/entity/director.entity'
+import { Genre } from '../genre/entities/genre.entity'
+import { MovieDetail } from './entity/movie-detail.entity'
+import { DataSource } from 'typeorm'
+import { MovieUserLike } from './entity/movie-user-like.entity'
 
 describe('MovieController (e2e)', () => {
   let app: INestApplication<App>
+  let dataSource: DataSource
 
-  beforeEach(async () => {
+  let users: User[]
+  let movies: Movie[]
+  let directors: Director[]
+  let genres: Genre[]
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
@@ -25,7 +38,88 @@ describe('MovieController (e2e)', () => {
       }),
     )
     await app.init()
+
+    /** 테스트용 DataSource 가져오기 */
+    dataSource = app.get<DataSource>(DataSource)
+
+    /** 테스트용 엔티티 Repository 가져오기 */
+    const userRepository = dataSource.getRepository(User)
+    const movieRepository = dataSource.getRepository(Movie)
+    const movieDetailRepository = dataSource.getRepository(MovieDetail)
+    const movieUserLikeRepository = dataSource.getRepository(MovieUserLike)
+    const directorRepository = dataSource.getRepository(Director)
+    const genreRepository = dataSource.getRepository(Genre)
+
+
+    /** 데이터 초기화 - delete/clear/truncate */
+    /*
+    await genreRepository.clear()
+    await directorRepository.clear()
+    await movieUserLikeRepository.clear()
+    await movieDetailRepository.clear()
+    await movieRepository.clear()
+    await userRepository.clear()
+    */
+    await dataSource.query(`
+      TRUNCATE TABLE
+        movie_user_like,
+        movie_detail,
+        movie,
+        genre,
+        director,
+        "user"
+      RESTART IDENTITY CASCADE;
+    `)
+
+    /** 테스트용 엔티티 생성 및 저장 */
+    users = [1, 2].map((number) =>
+      userRepository.create({
+        id: number,
+        email: `user${number}@test.com`,
+        password: `password${number}`,
+      }),
+    )
+    await userRepository.save(users)
+
+    directors = [1, 2].map((number) =>
+      directorRepository.create({
+        id: number,
+        dob: new Date(1990, 1, number),
+        nationality: `country${number}`,
+        name: `director${number}`,
+      }),
+    )
+    await directorRepository.save(directors)
+
+    genres = [1, 2].map((number) =>
+      genreRepository.create({
+        id: number,
+        name: `genre${number}`,
+        description: `description ${number}`,
+      }),
+    )
+    await genreRepository.save(genres)
+
+    movies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((number) =>
+      movieRepository.create({
+        id: number,
+        title: `title${number}`,
+        creator: users[0],
+        genres: genres,
+        likeCount: 0,
+        dislikeCount: 0,
+        detail: movieDetailRepository.create({
+          detail: `movie detail${number}`,
+        }),
+        movieFilePath: `movieFilePath${number}`,
+        director: directors[0],
+        createdAt: new Date(`2021-9-${number}`),
+      }),
+    )
+    await movieRepository.save(movies)
   })
+
+  beforeEach(async () => {})
 
   afterAll(async () => {
     /** 테스트 이후 NestJS 앱 종료 처리 */
